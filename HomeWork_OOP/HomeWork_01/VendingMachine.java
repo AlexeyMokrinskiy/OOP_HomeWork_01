737bdff028482a8c307a753a439d13446249cef2
AlexeyMokrinskiy <ambelimo@yandex.ru>
2023-02-18 17:19:36 +0300

    Home work done

---
+++ HomeWork_OOP/HomeWork_01/VendingMachine.java
@@ -0,0 +1,8 @@
+package HomeWork_OOP.HomeWork_01;
+
+public abstract class VendingMachine {
+
+    abstract void acceptingMoney();
+    abstract void productDelivery();
+    abstract void deliveryOfChange();
+}
